2a403e5a9b5db4c140ea22d95002fe54090963c1
zaking <521128056@qq.com>
2024-04-29 17:25:49 +0800

    白月光页面基本完成，待完善细节

--- zw-moonlight/app.vue
+++ zw-moonlight/app.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="app-box">
-    <NuxtPage/>
+    <div class="header-menu">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+      >
+        <el-menu-item index="1">
+          <NuxtLink to="/"> 首页 </NuxtLink>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <NuxtLink to="/product"> 产品介绍 </NuxtLink>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <el-backtop :right="100" :bottom="100" />
+    <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from "vue";
+const activeIndex = ref("1");
+</script>
+<style lang="less">
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
+<style lang="less" scoped>
+li a {
+  text-decoration: none;
+}
+</style>
---
+++ zw-moonlight/components/header-nav/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">
+    <h2>{{ $props.title }}</h2>
+    <el-divider />
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+});
+</script>
