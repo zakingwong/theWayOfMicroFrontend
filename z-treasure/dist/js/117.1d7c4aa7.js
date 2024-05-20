88d291cd2e396a1a8e441b50d8b3255ece1750d3
zaking <521128056@qq.com>
2024-05-20 17:44:00 +0800

    fix: 删除dist并增加gitignore配置

--- z-treasure/dist/js/117.1d7c4aa7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkz_treasure"]=self["webpackChunkz_treasure"]||[]).push([[117],{6117:function(e,n,r){r.r(n),r.d(n,{default:function(){return f}});var u=function(){var e=this,n=e._self._c;return n("div",{staticClass:"work-bench"},[e._v("工作台")])},t=[],s={name:"WorkBench"},l=s,a=r(1656),c=(0,a.A)(l,u,t,!1,null,null,null),f=c.exports}}]);
-//# sourceMappingURL=117.1d7c4aa7.js.map
