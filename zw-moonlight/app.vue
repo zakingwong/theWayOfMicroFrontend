facc23c76fe63afafc6b2df91c36e234acbe4256
zaking <521128056@qq.com>
2024-05-24 14:47:57 +0800

    feat:白月光项目增加导航

--- zw-moonlight/app.vue
+++ zw-moonlight/app.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="app-box">
-    测试自动构建
     <div class="header-menu">
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-      >
-        <el-menu-item index="1">
-          <NuxtLink to="/"> 首页 </NuxtLink>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <NuxtLink to="/product"> 产品介绍 </NuxtLink>
-        </el-menu-item>
-      </el-menu>
+      <div class="menu-part">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+        >
+          <el-menu-item index="1">
+            <NuxtLink to="/"> 首页 </NuxtLink>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <NuxtLink to="/product"> 产品介绍 </NuxtLink>
+          </el-menu-item>
+        </el-menu>
+      </div>
+
+      <div class="right-link">
+        <a
+          href="http://treasure.zaking.cn"
+          target="_blank"
+          rel="noopener noreferrer"
+          >超级无敌小芒果SAAS系统</a
+        >
+      </div>
     </div>
     <el-backtop :right="100" :bottom="100" />
     <NuxtPage />
@@ -31,6 +41,16 @@ body {
 }
 </style>
 <style lang="less" scoped>
+.header-menu {
+  display: flex;
+  justify-content: center;
+  .menu-part{
+    width:80vw;
+  }
+  .right-link{
+    line-height: 60px;
+  }
+}
 li a {
   text-decoration: none;
 }
