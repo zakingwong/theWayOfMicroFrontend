39f7f8965dd3504c37328e69a258f7c557173b83
zaking <521128056@qq.com>
2024-05-29 15:57:29 +0800

    fix: 修改名称

--- moonlight-ad/build.sh
+++ moonlight-ad/build.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# 检查是否存在 moonlightAd-container 容器，如果存在则停止并删除
-if docker ps -a | grep -q "moonlightAd-container"; then
-    echo "Stopping and removing moonlightAd-container container..."
-    docker stop moonlightAd-container
-    docker rm moonlightAd-container
-    echo "moonlightAd-container container removed."
+# 检查是否存在 moonlight-ad-container 容器，如果存在则停止并删除
+if docker ps -a | grep -q "moonlight-ad-container"; then
+    echo "Stopping and removing moonlight-ad-container container..."
+    docker stop moonlight-ad-container
+    docker rm moonlight-ad-container
+    echo "moonlight-ad-container container removed."
 else
-    echo "No moonlightAd-container container found."
+    echo "No moonlight-ad-container container found."
 fi
 
-# 检查是否存在 moonlightAd-image 镜像，如果存在则删除
-if docker images -q moonlightAd-image; then
-    echo "Removing moonlightAd-image image..."
-    docker rmi moonlightAd-image
-    echo "moonlightAd-image image removed."
+# 检查是否存在 moonlight-ad-image 镜像，如果存在则删除
+if docker images -q moonlight-ad-image; then
+    echo "Removing moonlight-ad-image image..."
+    docker rmi moonlight-ad-image
+    echo "moonlight-ad-image image removed."
 else
-    echo "No moonlightAd-image image found."
+    echo "No moonlight-ad-image image found."
 fi
 
-# 构建 moonlightAd-image 镜像
-echo "Building moonlightAd-image image..."
-docker build -t moonlightAd-image .
-echo "moonlightAd-image image built."
+# 构建 moonlight-ad-image 镜像
+echo "Building moonlight-ad-image image..."
+docker build -t moonlight-ad-image .
+echo "moonlight-ad-image image built."
 
-# 启动 moonlightAd-container 容器
-echo "Starting moonlightAd-container container..."
-docker run -d --name moonlightAd-container -p 92:3000 moonlightAd-image
-echo "moonlightAd-container container started."
+# 启动 moonlight-ad-container 容器
+echo "Starting moonlight-ad-container container..."
+docker run -d --name moonlight-ad-container -p 92:3000 moonlight-ad-image
+echo "moonlight-ad-container container started."
