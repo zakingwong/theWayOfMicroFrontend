996762d62392a2308029aacead1a3d5be5151e8e
zaking <521128056@qq.com>
2024-06-01 15:06:18 +0800

    fix: 细节处理

--- treasure-design/src/router/index.ts
+++ treasure-design/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/create-page',
