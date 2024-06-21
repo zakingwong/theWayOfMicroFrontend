eb0e8dd0ed0e8744be358a2b7898ab210dcbe690
zaking <521128056@qq.com>
2024-06-21 15:46:48 +0800

    feat: 本地示例基本完成

--- packages/main-react-app/src/React.js
+++ packages/main-react-app/src/React.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+const { mount, unmount } = require("micro-react-app");
 
 const containerId = 'react-app';
+
 function ReactApp() {
-  return <div id={containerId}>react</div>;
+  useEffect(() => {
+    mount(containerId);
+    return () => {
+      unmount();
+    };
+  }, []);
+  return <div id={containerId}></div>;
 }
 
 export default React.memo(ReactApp);
